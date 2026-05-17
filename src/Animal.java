220e780357443f893942be7eb71f8d08d9c8b9de
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2026-05-17 11:27:37 -0700

    Abstract Class constructor added, subclasses extending superclass animal added

--- src/Animal.java
+++ src/Animal.java
@@ -1,2 +1,13 @@
-public class Animal {
+abstract class Animal {
+    protected String name;
+    protected int age;
+    public  Animal(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public abstract void makeSound ();
+
+    public abstract void move();
+
+    public abstract void displayInfo();
 }
